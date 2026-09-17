da6e5633d08c467aa068ae6b8ccb082dc190b3ae
bkdargan <bkdargan@gmail.com>
2026-09-17 14:58:40 -0400

    Create Create-TestCalendarEvent.ps1

---
+++ scripts/Create-TestCalendarEvent.ps1
@@ -0,0 +1,10 @@
+Write-Host "Creating Test Calendar Event"
+
+Write-Host "Calendar ID Available:"
+Write-Host ($env:GOOGLE_CALENDAR_ID.Length)
+
+Write-Host "Credentials Available:"
+Write-Host ($env:GOOGLE_CREDENTIALS.Length)
+
+Write-Host ""
+Write-Host "Next step will be Google Calendar API write."
