56fd1517b5e8b6ceb93ea13dab86c11f5187dbca
bkdargan <bkdargan@gmail.com>
2026-09-17 14:13:45 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -3,23 +3,24 @@ Write-Host "Brunswick Event Object"
 $url = "https://www.ncbrunswick.com/event/ocean-isle-beach-summer-concert-series/2087/"
 
 $response = Invoke-WebRequest -Uri $url
+
 $content = $response.Content
 
-$titleLine = ($content -split "`n") | Where-Object {
-    $_ -match "<title>"
-} | Select-Object -First 1
+$titleLine = ($content -split "`n") |
+    Where-Object { $_ -match "<title>" } |
+    Select-Object -First 1
 
-$descLine = ($content -split "`n") | Where-Object {
-    $_ -match 'meta name="description"'
-} | Select-Object -First 1
+$title = $titleLine `
+    -replace "<title>", "" `
+    -replace "</title>", ""
 
 Write-Host ""
 Write-Host "======================="
 Write-Host "EVENT RECORD"
 Write-Host "======================="
-Write-Host $titleLine
 Write-Host ""
-Write-Host $descLine
+Write-Host "TITLE:"
+Write-Host $title
 Write-Host ""
 Write-Host "URL:"
 Write-Host $url
