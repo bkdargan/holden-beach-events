0a660724716ce43146dfb2968cb91ade8be14cdf
bkdargan <bkdargan@gmail.com>
2026-09-17 14:10:51 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -1,10 +1,24 @@
-Write-Host "Saving Event Page"
+Write-Host "Brunswick Event Extractor"
 
 $url = "https://www.ncbrunswick.com/event/ocean-isle-beach-summer-concert-series/2087/"
 
 $response = Invoke-WebRequest -Uri $url
 
-$response.Content | Out-File event-page.html
+$content = $response.Content
 
-Write-Host "Event page saved"
-Write-Host "Length: $($response.Content.Length)"
+$lines = $content -split "`n"
+
+foreach ($line in $lines) {
+
+    if ($line -like "*<title>*") {
+        Write-Host ""
+        Write-Host "TITLE:"
+        Write-Host $line
+    }
+
+    if ($line -like '*meta name="description"*') {
+        Write-Host ""
+        Write-Host "DESCRIPTION:"
+        Write-Host $line
+    }
+}
