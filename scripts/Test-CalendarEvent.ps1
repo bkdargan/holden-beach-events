dbf7619342d1277acaf4af417f5b6dc26a91683f
bkdargan <bkdargan@gmail.com>
2026-09-17 14:53:19 -0400

    Create Test-CalendarEvent.ps1

---
+++ scripts/Test-CalendarEvent.ps1
@@ -0,0 +1,9 @@
+Write-Host "Calendar Write Test"
+
+Write-Host ""
+Write-Host "Calendar ID Found:"
+Write-Host $env:GOOGLE_CALENDAR_ID
+
+Write-Host ""
+Write-Host "Credentials Length:"
+Write-Host $env:GOOGLE_CREDENTIALS.Length
