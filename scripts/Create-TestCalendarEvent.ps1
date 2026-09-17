2f5a8324ea04d811b9dc70d770bd76736e62834f
bkdargan <bkdargan@gmail.com>
2026-09-17 15:10:32 -0400

    Update Create-TestCalendarEvent.ps1

--- scripts/Create-TestCalendarEvent.ps1
+++ scripts/Create-TestCalendarEvent.ps1
@@ -1,15 +1,14 @@
-Write-Host "Creating Calendar Event Payload"
+Write-Host "Creating calendar event object"
 
 $event = @{
     summary = "Holden Beach Automation Test"
-    description = "Created automatically by GitHub Actions"
+    description = "Created automatically from GitHub Actions"
     location = "Holden Beach, NC"
+    date = (Get-Date).AddDays(1).ToString("yyyy-MM-dd")
 }
 
 $event | ConvertTo-Json | Out-File test-event.json
 
 Write-Host ""
-Write-Host "Created test-event.json"
-
-Write-Host ""
-Get-Content test
+Write-Host "Created:"
+Get-Content test-event.json
