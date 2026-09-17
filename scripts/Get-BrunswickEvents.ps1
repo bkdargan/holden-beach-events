80934aac55ea83d254ffda91f3699da969ebcf78
bkdargan <bkdargan@gmail.com>
2026-09-17 15:36:13 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -1,27 +1,10 @@
-Write-Host "Finding Brunswick Event URLs"
+Write-Host "Inspecting Brunswick Events HTML"
 
 $listingPage = Invoke-WebRequest -Uri "https://www.ncbrunswick.com/events/"
 
 $content = $listingPage.Content
 
-$lines = $content -split "`n"
-
-$count = 0
-
-foreach ($line in $lines)
-{
-    if ($line -like "*https://www.ncbrunswick.com/event/*")
-    {
-        Write-Host $line
-
-        $count++
-
-        if ($count -ge 10)
-        {
-            break
-        }
-    }
-}
-
-Write-Host ""
-Write-Host "Done"
+$content |
+    Select-String `
+    -Pattern "/event/" `
+    -Context 0,2
