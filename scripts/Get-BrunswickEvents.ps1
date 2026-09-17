67adbb3e5c76c8f10e2fbdfda403fb3d63ce6af7
bkdargan <bkdargan@gmail.com>
2026-09-17 15:37:19 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -1,10 +1,31 @@
-Write-Host "Inspecting Brunswick Events HTML"
+Write-Host "Finding Event Links"
 
 $listingPage = Invoke-WebRequest -Uri "https://www.ncbrunswick.com/events/"
 
 $content = $listingPage.Content
 
-$content |
-    Select-String `
-    -Pattern "/event/" `
-    -Context 0,2
+$pattern = '/event/[^"]+'
+
+$matches = :Matches($content, $pattern)
+
+Write-Host "Matches Found: $($matches.Count)"
+
+$urls = @()
+
+foreach ($match in $matches)
+{
+    $url = "https://www.ncbrunswick.com" + $match.Value
+
+    if ($urls -notcontains $url)
+    {
+        $urls += $url
+    }
+}
+
+Write-Host ""
+Write-Host "Unique URLs Found: $($urls.Count)"
+Write-Host ""
+
+$urls |
+    Select-Object -First 20 |
+    ForEach-Object { Write-Host $_ }
