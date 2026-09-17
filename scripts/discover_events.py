1178989ba256343c8389548299997a0e772a0392
bkdargan <bkdargan@gmail.com>
2026-09-17 15:54:06 -0400

    Update discover_events.py

--- scripts/discover_events.py
+++ scripts/discover_events.py
@@ -1,17 +1,31 @@
 import requests
 
-url = "https://www.ncbrunswick.com/events/"
+sites = [
+    "https://www.carolinabreezevacations.com/holden-beach-area-events",
+    "https://hbtownhall.com/parks-%26-recreation",
+    "https://www.townplanner.com/holden-beach/nc/",
+    "https://holdenbeachnc.com/holden-beach-concert-schedule/",
+    "https://www.coastalvacationresorts.com/holden-beach-events",
+    "https://www.hobbsrealty.com/holden-beach-events",
+    "https://www.eventbrite.com/d/nc--holden-beach/events/",
+    "https://www.ncbrunswick.com/events/"
+]
 
-response = requests.get(
-    url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
-)
+for site in sites:
+    print("\n" + "=" * 80)
+    print(site)
 
-print("Status Code:", response.status_code)
-print("Final URL:", response.url)
-print("Length:", len(response.text))
+    try:
+        response = requests.get(
+            site,
+            headers={
+                "User-Agent": "Mozilla/5.0"
+            },
+            timeout=30
+        )
 
-print("\nFIRST 1000 CHARACTERS:\n")
-print(response.text[:1000])
+        print("Status Code:", response.status_code)
+        print("Length:", len(response.text))
+
+    except Exception as ex:
+        print("ERROR:", ex)
