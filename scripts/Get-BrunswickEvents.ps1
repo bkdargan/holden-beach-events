56e84837b7801f4eec76e15600d482a014bc0904
bkdargan <bkdargan@gmail.com>
2026-09-17 13:56:08 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -6,22 +6,7 @@ $response = Invoke-WebRequest -Uri $url
 
 $content = $response.Content
 
-Write-Host ""
-Write-Host "Searching for title..."
-
-$patterns = @(
-    "Ocean Isle Beach Summer Concert Series",
-    "Town Center Park",
-    "September",
-    "6:30 PM"
-)
-
-foreach ($pattern in $patterns) {
-
-    if ($content.Contains($pattern)) {
-        Write-Host "FOUND: $pattern"
-    }
-    else {
-        Write-Host "NOT FOUND: $pattern"
-    }
-}
+$content | Select-String `
+    -Pattern "Ocean Isle Beach Summer Concert Series|Town Center Park|6:30 PM|September" `
+    -Context 2,2
+``
