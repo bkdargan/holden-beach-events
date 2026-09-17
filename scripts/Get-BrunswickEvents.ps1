00afef5edaef55b2654e39917ac357a7e95875aa
bkdargan <bkdargan@gmail.com>
2026-09-17 14:04:16 -0400

    Update Get-BrunswickEvents.ps1

--- scripts/Get-BrunswickEvents.ps1
+++ scripts/Get-BrunswickEvents.ps1
@@ -1,4 +1,4 @@
-Write-Host "Brunswick Event Metadata Test"
+Write-Host "Brunswick Event Object Test"
 
 $url = "https://www.ncbrunswick.com/event/ocean-isle-beach-summer-concert-series/2087/"
 
@@ -6,7 +6,14 @@ $response = Invoke-WebRequest -Uri $url
 
 $content = $response.Content
 
-$content |
-    Select-String `
-    -Pattern 'meta name="description"|<title>' `
-    -AllMatches
+$title = ($content | Select-String "<title>").Matches.Value
+
+$description = ($content | Select-String 'meta name="description"').Matches.Value
+
+Write-Host ""
+Write-Host "TITLE:"
+Write-Host $title
+
+Write-Host ""
+Write-Host "DESCRIPTION:"
+Write-Host $description
