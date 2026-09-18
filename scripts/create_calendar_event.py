64175c633e5cb43a9c0e69dd4c143026591ed8e9
bkdargan <bkdargan@gmail.com>
2026-09-18 10:45:09 -0400

    Update create_calendar_event.py

--- scripts/create_calendar_event.py
+++ scripts/create_calendar_event.py
@@ -129,7 +129,7 @@ for item in events:
             "🛍️ ",
             "🧘 ",
             "🏝️ "
-        \]:
+        ]:
             existing_title = existing_title.replace(
                 emoji
                 ""
