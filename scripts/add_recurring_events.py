90ce713fca65f978b82834193c1f402370b37443
bkdargan <bkdargan@gmail.com>
2026-09-23 13:32:50 -0400

    Fix syntax errors in weekday checks for events

--- scripts/add_recurring_events.py
+++ scripts/add_recurring_events.py
@@ -28,7 +28,7 @@ current = today
 
 while current <= end_date:
 
-    if current.weekday() in [0, 2, 4\]:
+    if current.weekday() in [0, 2, 4]:
 
         events.append(
             {
@@ -84,7 +84,7 @@ current = today
 
 while current <= end_date:
 
-    if current.weekday() in [1, 4, 5\]:
+    if current.weekday() in [1, 4, 5]:
 
         events.append(
             {
